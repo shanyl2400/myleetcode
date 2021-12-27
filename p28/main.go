2e78a0897010ffd9a9470a3b76f1a97978efe27d
shanyl2400 <white.shan@badanamu.com.cn>
2021-12-27 16:50:15 +0800

    update p28

--- p28/main.go
+++ p28/main.go
@@ -3,27 +3,49 @@ package main
 import "fmt"
 
 func strStr(haystack string, needle string) int {
-	if haystack == "" && needle == "" {
+	if needle == "" {
 		return 0
 	}
-	j := 0
+	if haystack == "" {
+		return -1
+	}
+	k := -1
+	pi := computePrefix(needle)
 	for i := range haystack {
-		j = 0
-		for j < len(needle) && i+j < len(haystack) && haystack[i+j] == needle[j] {
-			j++
+		for k >= 0 && haystack[i] != needle[k+1] {
+			k = pi[k]
 		}
-		if len(needle) == j {
-			return i
+		if haystack[i] == needle[k+1] {
+			k++
+		}
+		if k == len(needle)-1 {
+			return i - len(needle) + 1
 		}
 	}
 	return -1
 }
+func computePrefix(p string) []int {
+	pi := make([]int, len(p))
+	pi[0] = -1
+	k := -1
+
+	for i := 1; i < len(p); i++ {
+		for k >= 0 && p[i] != p[k+1] {
+			k = pi[k]
+		}
+		if p[k+1] == p[i] {
+			k++
+		}
+		pi[i] = k
+	}
+	return pi
+}
 
 func main() {
-	// haystack := "hello"
-	// needle := "ll"
-	haystack := "aaaaa"
-	needle := "bba"
+	haystack := "hello"
+	needle := "ll"
+	// haystack := "aaaaa"
+	// needle := "bba"
 	// haystack := "aaa"
 	// needle := "aaaa"
 	fmt.Println(strStr(haystack, needle))
