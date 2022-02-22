4eee8fe5765116a228c3ebb5a8af468de440b323
shanyl2400 <white.shan@badanamu.com.cn>
2022-02-22 14:00:51 +0800

    add p295

---
+++ p295/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"container/heap"
+	"sort"
+)
+
+type MedianFinder struct {
+	minQ, maxQ hp
+}
+
+func Constructor() MedianFinder {
+	return MedianFinder{}
+}
+
+func (this *MedianFinder) AddNum(num int) {
+	maxQ, minQ := &this.maxQ, &this.minQ
+	if minQ.Len() == 0 || num <= -minQ.IntSlice[0] {
+		heap.Push(minQ, -num)
+		if maxQ.Len()+1 < minQ.Len() {
+			heap.Push(maxQ, -heap.Pop(minQ).(int))
+		}
+	} else {
+		heap.Push(maxQ, num)
+		if maxQ.Len() > minQ.Len() {
+			heap.Push(minQ, -heap.Pop(maxQ).(int))
+		}
+	}
+}
+
+func (this *MedianFinder) FindMedian() float64 {
+	minQ, maxQ := this.minQ, this.maxQ
+	if minQ.Len() > maxQ.Len() {
+		return float64(-minQ.IntSlice[0])
+	}
+	return float64(maxQ.IntSlice[0]-minQ.IntSlice[0]) / 2
+}
+
+type hp struct {
+	sort.IntSlice
+}
+
+func (h *hp) Push(v interface{}) {
+	h.IntSlice = append(h.IntSlice, v.(int))
+}
+func (h *hp) Pop() interface{} {
+	a := h.IntSlice
+	v := a[len(a)-1]
+	h.IntSlice = a[:len(a)-1]
+	return v
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.AddNum(num);
+ * param_2 := obj.FindMedian();
+ */
